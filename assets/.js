1a4ce167cb85636d0a9d189911b3cbd8e512a22d
Hannah <heenan93@gmail.com>
2022-05-01 13:59:30 -0700

    add flex to CSS, added var in js

---
+++ assets/.js
@@ -0,0 +1,20 @@
+// start time variable
+let secondsLeft= 100;
+
+// score count
+let score = 0
+
+// question count 
+var questionCount = 0;
+
+// element timer
+let timer = document.getElementById("timer")
+
+// timer start on click of button
+function setTime () {
+    displayQuestions();
+
+}
+// div for answers and results
+let answers = document.getElementById("results");
+var choices = documents.getElementById("choices");
