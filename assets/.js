a2a20e8edfdc12822b5247e08581df0df2991130
Hannah <heenan93@gmail.com>
2022-05-01 14:28:08 -0700

    add questions to js

--- assets/.js
+++ assets/.js
@@ -20,3 +20,38 @@ let questionDiv = document.getElementById("question-div");
 // div for answers and results
 let answers = document.getElementById("results");
 var choices = documents.getElementById("choices");
+
+// alt styling
+function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
+    function showQuestions (questions, quizContainer,){
+        // code here
+    }
+}
+function showResults(questions, quizContainer, resultsContainer){
+    // code here
+}
+showQuestions (questions, quizContainer);
+
+// on click show results
+submitButoon.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+}
+
+var myQuestions = [
+    {
+        question: "What is console.log used for in JavaScript?",
+        answers: {
+            a:'log the console', 
+            b:'display error in console', 
+            c:'diagnose or troubleshoot minor issues',
+            d: 'same as return',
+        },
+    correctAnswer: 'c, "diagnose or troubleshoot minor issues'
+    },
+    {
+        question: "What is the purpose of an empty array?",
+        answers: {
+            
+        }
+    }
+]
